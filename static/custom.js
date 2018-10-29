760c6ee17f7e9e30ed2f6329c647d4d51404c0a5
Caleb Robinson <dcrobins@gatech.edu>
2018-10-29 10:20:53 -0700

    0. Added collapsible tree

---
+++ static/custom.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.treeNode').hide()
+
+    $("#superRootNode").find("li").each(function(){
+        $(this).css("list-style","none");
+    });
+
+    $("#superRootNode").find("li").each(function(){
+
+        if($(this).next().children().length == 0){
+            $(this).prepend("[-] ");
+        }else{
+            $(this).prepend("[+] ");
+        }
+    });
+
+    $('.rootNode').click(function() {
+        $(this).next().children("li").slideToggle();
+        $(this).siblings("li").next().find("li").slideUp();
+    });
+
+    $('.treeNode').click(function() {
+        $(this).next().children("li").slideToggle();
+        $(this).siblings("li").next().find("li").slideUp();
+    });
+})
